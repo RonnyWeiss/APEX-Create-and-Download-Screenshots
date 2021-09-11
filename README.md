dcd46301131f42e171b2e0ea2f2d9f8dc9e2092f
Ronny Weiss <30749177+RonnyWeiss@users.noreply.github.com>
2021-09-11 20:53:36 +0200

    initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# APEX-Create-and-Download-Screenshots
+ # APEX-Create-and-Download-Screenshots
+
+![Screenshot](https://raw.githubusercontent.com/RonnyWeiss/APEX-Create-and-Download-Screenshots/main/screenshot.gif)
+
 This plug-in is used to create and download screenshots from a page elements using html2canvas libary. To hide elements in screenshot just add "data-html2canvas-ignore" into the element attributes.
+
+Examples can be found in the Sample App:
+
+https://apex.oracle.com/pls/apex/f?p=103428
+
+[![Linktr.ee](https://img.shields.io/badge/Linktr.ee-ronny.weiss-%2339e09b)](https://linktr.ee/ronny.weiss) [![Twitter URL](https://img.shields.io/twitter/url?style=social&url=https%3A%2F%2Ftwitter.com%2Fronny_weiss_)](https://twitter.com/ronny_weiss_) [![YouTube](https://img.shields.io/badge/-YouTube-%23ff0000)](https://www.youtube.com/channel/UCsqbWnOolAitsbIz517styA)
+
+If you like my stuff, donate me a coffee or checkout my Amazon Wishlist
+
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.me/RonnyW1) or [![Donate](https://img.shields.io/badge/Amazon-Wishlist-%23febd69)](https://www.amazon.de/hz/wishlist/ls/1VFULRV8XUPQ0?ref_=wl_share)
---
+++ src/js/script.js
@@ -0,0 +1,88 @@
+function apexCreateScreenshotPlugin(pSelf, pFileName) {
+    "use strict";
+    var util = {
+        featureDetails: {
+            name: "APEX Create and Download Screenshots",
+            scriptVersion: "1.0.0.0",
+            utilVersion: "1.6",
+            url: "https://github.com/RonnyWeiss",
+            license: "MIT"
+        },
+        loader: {
+            start: function (id, setMinHeight) {
+                if (setMinHeight) {
+                    $(id).css("min-height", "100px");
+                }
+                apex.util.showSpinner($(id));
+            },
+            stop: function (id, removeMinHeight) {
+                if (removeMinHeight) {
+                    $(id).css("min-height", "");
+                }
+                $(id + " > .u-Processing").remove();
+                $(id + " > .ct-loader").remove();
+            }
+        },
+        isDefinedAndNotNull: function (pInput) {
+            if (typeof pInput !== "undefined" && pInput !== null && pInput != "") {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+
+    var loadSel = "html";
+
+    apex.debug.info({
+        "fct": util.featureDetails.name + " - " + "apexCheckboxToggle",
+        "arguments": {
+            "pSelf": pSelf
+        },
+        "featureDetails": util.featureDetails
+    });
+
+    util.loader.start(loadSel);
+
+    //WORKAROUND: html2canvas has problems with svgs and box shadow
+    $.each($(pSelf.affectedElements), function (i, item) {
+        $(item).addClass("remove-box-shadows");
+        $(item).find("*").addClass("remove-box-shadows");
+    });
+
+    try {
+        // WORKAROUND: html2canvas starts to fast so loader is not rendered with short wait
+        setTimeout(function () {
+            $.each($(pSelf.affectedElements), function (i, item) {
+                html2canvas($(item)[0], {
+                    "logging": (apex.debug.getLevel() >= 4)
+                })
+                    .then(canvas => {
+                        var url = canvas.toDataURL();
+                        var downlo = $("<a>");
+                        downlo.attr("href", url);
+                        if (util.isDefinedAndNotNull(pFileName)) {
+                            downlo.attr("download", pFileName + ".png");
+                        } else {
+                            downlo.attr("download", "screenshot_" + new Date().toLocaleDateString() + ".png");
+                        }
+                        downlo.appendTo("body");
+                        util.loader.stop(loadSel);
+                        downlo[0].click();
+                        downlo.remove();
+                        // remove added box shadow helper class
+                        $(item).removeClass("remove-box-shadows");
+                        $(item).find("*").removeClass("remove-box-shadows");
+                    });
+            });
+        }, 50);
+    } catch (err) {
+        util.loader.stop(loadSel);
+        apex.debug.error({
+            "fct": util.featureDetails.name + " - " + " execute html2canvas",
+            "msg": "Error while to execute html2canvas",
+            "err": err,
+            "featureDetails": util.featureDetails
+        });
+    }
+}
